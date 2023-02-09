097f14db90778323dc89aa04080ebabc82118971
yicr <yicr@users.noreply.github.com>
2023-02-09 18:36:33 +0900

    feat(deps)!: update deps.

--- .projenrc.js
+++ .projenrc.js
@@ -6,20 +6,24 @@ const PROJECT_DESCRIPTION = 'This is an AWS CDK Construct to make deploying a Fr
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.43.0',
+  cdkVersion: '2.62.0',
   defaultReleaseBranch: 'main',
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
   keywords: ['aws', 'cdk', 'cloudfront', 'cdn', 'web', 'spa', 'deploy'],
   repositoryUrl: 'https://github.com/yicr/frontend-web-app-deploy-stack.git',
   npmAccess: javascript.NpmAccess.PUBLIC,
-  devDeps: [
-    '@yicr/secure-cloudfront-origin-bucket@^0.2.6',
+  deps: [
+    '@yicr/secure-cloudfront-origin-bucket@^0.2.8',
     '@yicr/secure-frontend-web-app-cloudfront-distribution@^0.3.6',
   ],
+  devDeps: [
+    '@yicr/secure-cloudfront-origin-bucket@0.2.8',
+    '@yicr/secure-frontend-web-app-cloudfront-distribution@0.3.6',
+  ],
   peerDeps: [
-    '@yicr/secure-cloudfront-origin-bucket@^0.2.6',
-    '@yicr/secure-frontend-web-app-cloudfront-distribution@^0.3.6',
+    '@yicr/secure-cloudfront-origin-bucket@0.2.8',
+    '@yicr/secure-frontend-web-app-cloudfront-distribution@0.3.6',
   ],
   depsUpgradeOptions: {
     workflowOptions: {
