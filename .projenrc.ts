e904a6c46453d190087887c7cf2d59cc4fa6e89d
yicr <yicr@users.noreply.github.com>
2023-08-15 20:46:24 +0900

    feat!: update projen setting(publish to maven)

--- .projenrc.ts
+++ .projenrc.ts
@@ -44,5 +44,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'gammarer.aws-frontend-web-app-deploy-stack',
     module: 'gammarer.aws_frontend_web_app_deploy_stack',
   },
+  publishToMaven: {
+    mavenGroupId: 'com.gammarer',
+    javaPackage: 'com.gammarer.cdk.aws.frontend_web_app_deploy_stack',
+    mavenArtifactId: 'aws-frontend-web-app-deploy-stack',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
 });
 project.synth();
