80d5fdf6b7990e014b8c540f2ae1a5eb5d6c6d24
yicr <yicr@users.noreply.github.com>
2025-04-17 14:32:41 +0900

    chore: workflow node version set 22.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -29,7 +29,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@gammarers/aws-secure-bucket@~1.4.1',
   ],
   minNodeVersion: '16.0.0',
-  workflowNodeVersion: '22.4.x',
+  workflowNodeVersion: '22.x',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
